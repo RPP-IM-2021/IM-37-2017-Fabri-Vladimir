e8f092dc26195bbbad132c083ecbe641a0026464
VladimiFabri98 <vfficial98@gmail.com>
2021-04-08 15:36:44 +0200

    Posle prvih vezbi, kreiran rest controller sa metodama helloWorld i zbir

--- rpp2021/src/main/java/rpp2021/Rpp2021Application.java
+++ rpp2021/src/main/java/rpp2021/Rpp2021Application.java
@@ -1,7 +1,12 @@
 package rpp2021;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Rpp2021Application {
@@ -9,5 +14,16 @@ public class Rpp2021Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Rpp2021Application.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Beans provided by spring boot:");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
 }
